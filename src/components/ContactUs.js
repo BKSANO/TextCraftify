8c651b84fcb80234e94da33685d423407b5552fc
Brajesh Sanodiya <brajesh.sanodiya@ril.com>
2023-08-17 22:57:21 +0530

    Update ContactUs.js

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,24 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 const ContactUs = (props) => {
   const [formStatus, setFormStatus] = React.useState('Send')
   let myStyle = {
     color: props.mode==="dark"?"white":"#333333",
     backgroundColor: props.mode==="dark"?"#333333":"white"
   }
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const onSubmit = (e) => {
     e.preventDefault()
     setFormStatus('Submitting...')
-    const { name, email, message } = e.target.elements
     let conFom = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
+      name: name,
+      email: email,
+      message: message,
     }
     console.log(conFom)
     setTimeout(() => {
         setFormStatus('Send')
         props.showAlert("Details Submitted! will reach you soon.","success")
-      }, 1500);
+      }, 3000);
 
   }
   return (
@@ -29,21 +32,21 @@ const ContactUs = (props) => {
           <label className="form-label" htmlFor="name">
             Name
           </label>
-          <input className="form-control" type="text" id="name" required />
+          <input className="form-control" type="text" id="name" onChange={(e) => setName(e.target.value)} required />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="email">
             Email
           </label>
-          <input className="form-control" type="email" id="email" required />
+          <input className="form-control" type="email" id="email" onChange={(e) => setEmail(e.target.value)}  required />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="message">
             Message
           </label>
-          <textarea className="form-control" id="message" required />
+          <textarea className="form-control" id="message" onChange={(e) => setMessage(e.target.value)}  required />
         </div>
-        <button className="btn btn-primary" type="submit">
+        <button disabled={name.length===0 && email.length===0 && message.length===0} className="btn btn-primary" type="submit">
           {formStatus}
         </button>
       </form>
